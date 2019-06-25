250833756cabb17983584e58bedc65d087dc18ee
Gaurav soni <Gaurav@allcadservices.com>
2019-06-25 23:29:25 +0400

    updated the package.json file

---
+++ build/precache-manifest.0aaef7ae554b3cb78dbe2036b3022a26.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "bcd569760e57b97c2c2bc8d6b7871243",
+    "url": "/static/media/mongodb.bcd56976.png"
+  },
+  {
+    "revision": "a0f75a0da9954d1b56c6",
+    "url": "/static/css/main.126da8fd.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e65ea0cc168349ff3298",
+    "url": "/static/js/2.e65ea0cc.chunk.js"
+  },
+  {
+    "revision": "ac498057da07fecc1e370616c0f37604",
+    "url": "/static/media/angular.ac498057.png"
+  },
+  {
+    "revision": "c6b2776244ab847ef3ed2b02566297b1",
+    "url": "/static/media/css.c6b27762.png"
+  },
+  {
+    "revision": "9aec778f24e19d2644b8948864476808",
+    "url": "/static/media/docker.9aec778f.png"
+  },
+  {
+    "revision": "fa06dc323bfa2d71f6be5860fe11ade3",
+    "url": "/static/media/git.fa06dc32.png"
+  },
+  {
+    "revision": "5f04a792d33dc086330c1bf89fc0c0e5",
+    "url": "/static/media/graphql.5f04a792.png"
+  },
+  {
+    "revision": "a8ba246755fb079c9742ab01fa05a323",
+    "url": "/static/media/heroku.a8ba2467.png"
+  },
+  {
+    "revision": "9606bd47c1a68273d39620c6b972938e",
+    "url": "/static/media/javascript.9606bd47.png"
+  },
+  {
+    "revision": "58716a40380db8075cc991ddccf19e3c",
+    "url": "/static/media/jest.58716a40.png"
+  },
+  {
+    "revision": "a0f75a0da9954d1b56c6",
+    "url": "/static/js/main.a0f75a0d.chunk.js"
+  },
+  {
+    "revision": "57204ee7e730950d8e613311fc845515",
+    "url": "/static/media/nodejs.57204ee7.png"
+  },
+  {
+    "revision": "63edc07ced3656d90d84ae881a5083af",
+    "url": "/static/media/react.63edc07c.png"
+  },
+  {
+    "revision": "669c0c0e214ce19aa22cefb2a825d487",
+    "url": "/static/media/scss.669c0c0e.png"
+  },
+  {
+    "revision": "fa68e0535ed92ca500af00055940a290",
+    "url": "/static/media/xd.fa68e053.png"
+  },
+  {
+    "revision": "6163476d4921dbaf83480bfe4690fb4e",
+    "url": "/static/media/facebook.6163476d.png"
+  },
+  {
+    "revision": "6976c96a4793fa60b9d370410bef647e",
+    "url": "/static/media/linkedin.6976c96a.png"
+  },
+  {
+    "revision": "211d1cb7ff532a9cff4981360a74b12b",
+    "url": "/static/media/email.211d1cb7.png"
+  },
+  {
+    "revision": "34352f2d45a02a8876cf175ec52f8d29",
+    "url": "/static/media/cv.34352f2d.png"
+  },
+  {
+    "revision": "16189645949d6d4a1558709f95ca2163",
+    "url": "/static/media/gaurav_cv.16189645.pdf"
+  },
+  {
+    "revision": "1c7b1b500016845e96728a44af7f72bb",
+    "url": "/static/media/wallpaper.1c7b1b50.jpg"
+  },
+  {
+    "revision": "e29f703075cb30f49fb0be30a3bf0040",
+    "url": "/index.html"
+  }
+];
